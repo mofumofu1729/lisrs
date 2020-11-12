237f945b16e307d2de920e038d16a7569ebee816
OKIMOTO Yusuke <okimoto@pcl.cs.waseda.ac.jp>
2020-11-12 23:14:26 +0900

    字句分割を関数に切り出す

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use std::io::{stdout, Write};
 
+fn tokenize(s: &str) -> Vec<String> {
+    let spreaded = s.replace("(", " ( ").replace(")", " ) ");
+    let tokens: Vec<String> = spreaded.split_whitespace().map(|item| item.to_string()).collect();
+
+    return tokens;
+}
+
 fn main() {
     print!("lis.rs> ");
     std::io::stdout().flush().unwrap();
@@ -7,11 +14,9 @@ fn main() {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).ok();
 
-    let answer = input.replace("(", " ( ").replace(")", " ) ");
+    let split = tokenize(&input);
 
-    let split: Vec<&str> = answer.split_whitespace().collect();
-
-    let op = split[1];
+    let op = &split[1];
     let a: i32 = split[2].parse().unwrap();
     let b: i32 = split[3].parse().unwrap();
 
